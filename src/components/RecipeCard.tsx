64b869469f073d028ae5353dad7116482bdabcc9
Reika Glare <reikaglare@gmail.com>
2025-12-28 14:46:40 +0100

    Aggiornamento: calorie locali, lista spesa e sostituzioni smart

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { Clock, Users, ChefHat, Heart } from 'lucide-react';
+import { Clock, Users, Heart } from 'lucide-react';
 
 interface RecipeCardProps {
     recipe: any;
